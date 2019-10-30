e367ed74761a8565af60309a4f0f43ee8a982dac
hayesch19 <chrishayespro@gmail.com>
2019-10-30 10:49:28 -0400

    Header Component Added

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Header from './components/header'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <Text>Hello World!</Text>
     </View>
   )
@@ -11,7 +13,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
